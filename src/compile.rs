297005b4b45c0f2a310b31443a2db6bd712f850f
KyteKode <231786375+KyteKode@users.noreply.github.com>
2026-01-27 09:46:10 -0500

    Added tests for argument handling

--- src/compile.rs
+++ src/compile.rs
@@ -85,7 +85,7 @@ pub fn handle_args(args: Vec<String>) -> CompileData {
         match arg.as_str() {
             "-o" => is_output_name = true,
             "--version" => data.version_flag = true,
-            "-v" | "--verbose" => data.verbose_flag = true,
+            "-v" => data.verbose_flag = true,
             "-L" => {
                 if data.output_type == OutputType::Parsed {
                     errors.push(BasmError::UndeterminedOutputType);
@@ -128,7 +128,105 @@ pub fn compile(args: Vec<String>) -> ! {
 # Flags:
 -o: Sets output filename. Argument immediately after is the name.
 --version: Block-ASM version and target Scratch version.
--v or --verbose: Prints more data during compilation.
+-v: Prints more data during compilation.
 -L: Returns lexed output.
 -P: Returns parsed output.
  */
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // MUST RUN IN Block-ASM/ DIRECTORY
+    #[test]
+    fn handle_source_arg() {
+        let names = ["tests/abcde.txt", "tests/subdirectory/../hello.txt", "tests/subdirectory/../subdirectory/test.txt"].iter();
+
+        let mut files_data = vec!["abcdefghijklmnopqrstuvwxyz test"];
+        if cfg!(windows) {
+            files_data.push("Hello, world!\r\nThis is a test!");
+            files_data.push("testing\r\nTesting\r\n123");
+        } else {
+            files_data.push("Hello, world!\nThis is a test!");
+            files_data.push("testing\nTesting\n123");
+        }
+
+        for (name, file_data) in names.zip(files_data.iter()) {
+            let data = handle_args(vec![
+                "basm".to_string(),
+                name.to_string(),
+            ]);
+
+            assert_eq!(data.source, file_data.to_string());
+        }
+    }
+
+    #[test]
+    fn handle_output_name() {
+        let names = ["test.sb3", "myProject123.sb3", "Unnamed-2019.sb3"].iter();
+
+        for name in names {
+            let data = handle_args(vec![
+                "basm".to_string(),
+                "tests/abcde.txt".to_string(),
+                "-o".to_string(),
+                name.to_string()
+            ]);
+
+            assert_eq!(data.output_name, name.to_string());
+        }
+    }
+
+    #[test]
+    fn handle_output_type() {
+        let data = handle_args(vec![
+            "basm".to_string(),
+            "tests/abcde.txt".to_string(),
+            "-L".to_string()
+        ]);
+
+        assert_eq!(data.output_type,OutputType::Lexed);
+
+        let data = handle_args(vec![
+            "basm".to_string(),
+            "tests/abcde.txt".to_string(),
+            "-P".to_string()
+        ]);
+
+        assert_eq!(data.output_type, OutputType::Parsed);
+    }
+
+    #[test]
+    fn handle_verbose_flag() {
+        let values = [true, false];
+        for is_verbose in values.iter() {
+            let mut args = vec![
+                "basm".to_string(),
+                "tests/abcde.txt".to_string()
+            ];
+            if *is_verbose {
+                args.push("-v".to_string());
+            }
+            let data = handle_args(args);
+
+            assert_eq!(data.verbose_flag, *is_verbose);
+        }
+    }
+
+    #[test]
+    fn handle_version_flag() {
+        let values = [true, false];
+        for show_version in values.iter() {
+            let mut args = vec![
+                "basm".to_string(),
+                "tests/abcde.txt".to_string()
+            ];
+            if *show_version {
+                args.push("--version".to_string());
+            }
+            let data = handle_args(args);
+
+            assert_eq!(data.version_flag, *show_version);
+        }
+    }
+}
